dda06ed0448878dc3b543f66035f8996b90d1afa
Patasek23 <94843622+Patasek23@users.noreply.github.com>
2024-01-13 19:42:29 +0100

    Add files via upload

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Trampota_PM',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
